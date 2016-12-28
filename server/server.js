bc38ba0ffb7359cf6d5b866fd7df4a7e8e365027
Nizar Ayari <nizarayari17@gmail.com>
2016-12-28 13:51:12 -0800

    [deploy]put port at 8000

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ app.get('*', function(req, res) {
 
 
 //Server Setup
-const port = process.env.PORT || 3090;
+const port = process.env.PORT || 8000;
 const server = http.createServer(app);
 server.listen(port);
 console.log('Server listenning on: ', port);
