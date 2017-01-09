878afd7b5403a7e0140129c62c3de86091b1e3f2
Nizar Ayari <nizarayari17@gmail.com>
2017-01-10 00:11:23 +0100

    [refactor]used aws sdk instead of nodemailer

--- server/router.js
+++ server/router.js
@@ -1,5 +1,4 @@
-const nodemailer = require('nodemailer');
-const ses = require('nodemailer-ses-transport');
+const aws = require('aws-sdk');
 
 module.exports = function (app){
   
@@ -10,27 +9,58 @@ module.exports = function (app){
 
     const { name, email, message } = req.body
 
-    const pluginConfig = {
-      transporter: 'ses',
-      accessKeyId: process.env.accessKeyId,
-      secretAccessKey: process.env.secretAccessKey
-    };
+    aws.config.update({
+    accessKeyId: process.env.accessKeyId,
+    secretAccessKey: process.env.secretAccessKey,
+    region:'us-east-1'
+    }); 
+
+    // Instantiate SES.
+    const ses = new aws.SES({apiVersion: 'latest'});
 
     // Set Up Nodemailer then send email
-    const mailOptions = {
-      from:'"My website" <' + process.env.user + '>',
-      to: 'nizarayari17@gmail.com',
-      subject: 'Info',
-      text: 'You have an email from ' + name + ' ' + email + ' saying: ' + message 
-    };
+    
+    const mailOptions ={
+        Source: '"My website" <' + process.env.user + '>',
+        Destination: {
+          ToAddresses: [process.env.user]
+        },
+        Message: {
+          Subject: {
+            Data: 'Info'
+          },
+          Body: {
+            Html: {
+              Data: 'You have an email from ' + name + ' ' + email + ' saying: ' + message
+            }
+          }
+        }
+      }
 
-    const transport = nodemailer.createTransport(pluginConfig);
-    transport.sendMail(mailOptions, function(err, info) {
-      if (err) console.log(err);
-      console.log('Message sent: ' + info);
-      res.json({ success: true, message: 'Email has been successfuly sent.'});
-    });
+    // const mailOptions = {
+    //   from:'"My website" <' + process.env.user + '>',
+    //   to: 'nizarayari17@gmail.com',
+    //   subject: 'Info',
+    //   text: 'You have an email from ' + name + ' ' + email + ' saying: ' + message 
+    // };
+
+    // const transport = nodemailer.createTransport(ses(smtpConfig));
+    // transport.sendMail(mailOptions, function(err, info) {
+    //   if (err) console.log(err);
+    //   console.log('Message sent: ');
+    //   res.json({ success: true, message: 'Email has been successfuly sent.'});
+    // });
+
+    ses.sendEmail(mailOptions, function(err, data){
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Email sent:');
+        console.log(data);
+        res.json({ success: true, message: 'Email has been successfuly sent.'});
+      }
+    })
+
   })
-
 
 }
